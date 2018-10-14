c65fc672ce1f7201e97e5d48f49fea31c6a45886
adishare <adie.share@gmail.com>
2018-10-15 06:16:14 +0700

    test

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// mongoose.connect('mongodb://localhost:27017/h8',{ useNewUrlParser: true })
+mongoose.connect(`mongodb://${process.env.dbusername}:${process.env.dbpassword}@ds029630.mlab.com:29630/h8store`,{ useNewUrlParser: true })
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+app.use('/', require('./routes'));
+
+app.use(function(req, res, next) {
+	next(new Error("Not Found").status(404))
+})
+
+module.exports = app;
